91fd69bfb8a85758866b879867697b2e9f60f32b
Maroš Pohančeník <maros.pohancenik@medelogics.onmicrosoft.com>
2021-12-02 20:13:58 +0100

    Added EF6 and fix for missing SqlServer dll when app compiled

--- DAL/EmployeeEfDataContext.cs
+++ DAL/EmployeeEfDataContext.cs
@@ -18,7 +18,7 @@ namespace DAL
 		public DbSet<TASKSTATE> TASKSTATEs { get; set; }
 
 
-        public EmployeeEfDataContext() : base(ConnectionString)
+		public EmployeeEfDataContext() : base(ConnectionString)
 		{
 		}
 
@@ -43,7 +43,16 @@ namespace DAL
 			modelBuilder.Entity<TASK>().ToTable("TASK");
 
 			modelBuilder.Entity<TASKSTATE>().ToTable("TASKSTATE");
+		}
 
+
+		private void FixEfProviderServicesProblem()
+		{
+			// The Entity Framework provider type 'System.Data.Entity.SqlServer.SqlProviderServices, EntityFramework.SqlServer'
+			// for the 'System.Data.SqlClient' ADO.NET provider could not be loaded.
+			// Make sure the provider assembly is available to the running application.
+			// See http://go.microsoft.com/fwlink/?LinkId=260882 for more information.
+			var instance = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
 		}
 	}
 }
